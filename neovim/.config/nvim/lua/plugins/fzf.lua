34d1305e05d2e1fdc4d69e851cae9aab823ecbcf
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2025-10-15 16:54:50 +1100

    15/10/2025 (2)

--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -11,10 +11,9 @@ return {
 			require("fzf-lua").setup()
 			vim.keymap.set("n", "<leader>sh", require("fzf-lua").helptags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sf", require("fzf-lua").files, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>sb", require("fzf-lua").buffers, { desc = "[S]earch [B]uffers" })
-			vim.keymap.set("n", "<leader>sg", require("fzf-lua").live_grep, { desc = "[S]earch [G]rep" })
-			vim.keymap.set("n", "<leader>sr", require("fzf-lua").git_files, { desc = "[S]earch [R]oot" })
-			vim.keymap.set("n", "<leader>s.", require("fzf-lua").resume, { desc = "[S]earch resume" })
+			vim.keymap.set("n", "<leader>sb", require("fzf-lua").buffers, { de<Down>c = "[S]earch [B]uffers" })
+			vim.keymap.set("n", "sg", require("fzf-lua").live_grep, { desc = "[S]earch [G]rep" })
+			vim.keymap.set("n", "<leader>sg", require("fzf-lua").git_files, { desc = "[S]earch [R]oot" })
 			vim.keymap.set("n", "<leader>sa", require("fzf-lua").resume, { desc = "[S]earch [A]gain (recent)" })
 			vim.keymap.set("n", "<leader>q", require("fzf-lua").diagnostics_workspace, { desc = "[Q]uickfix" })
 		end,
--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -45,9 +45,7 @@ return { -- Adds git signs to the gutter, as well as utilities for making change
 				gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 			end, { desc = "git [r]eset hunk" })
 			-- normal mode
-			map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
 			map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
-			map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
 			map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
 			map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
 			map("n", "<leader>hb", gitsigns.blame_line, { desc = "git [b]lame line" })
